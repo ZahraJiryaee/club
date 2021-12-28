914e77390f5e948f89fd01d4b1d7aaaaae96e882
LAPTOP-UUH0CQCK\Shamim <sh.jozi@medrick.info>
2021-12-28 19:13:54 +0330

    bug fix - passing jsx to native element

--- src/components/DashboardLayout.js
+++ src/components/DashboardLayout.js
@@ -1,57 +1,56 @@
-import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import { experimentalStyled } from '@material-ui/core';
-import DashboardNavbar from './DashboardNavbar';
-import DashboardSidebar from './DashboardSidebar';
-
-const DashboardLayoutRoot = experimentalStyled('div')(
-  ({ theme }) => ({
-    backgroundColor: theme.palette.background.default,
-    display: 'flex',
-    height: '100%',
-    overflow: 'hidden',
-    width: '100%'
-  })
-);
-
-const DashboardLayoutWrapper = experimentalStyled('div')(
-  ({ theme, isDesktopNavOpen }) => ({
-    display: 'flex',
-    flex: '1 1 auto',
-    overflow: 'hidden',
-    paddingTop: 64,
-    [theme.breakpoints.up('lg')]: {
-      paddingRight: isDesktopNavOpen ? 256 : 0
-    }
-  })
-);
-
-const DashboardLayoutContainer = experimentalStyled('div')({
-  display: 'flex',
-  flex: '1 1 auto',
-  overflow: 'hidden'
-});
-
-const DashboardLayoutContent = experimentalStyled('div')({
-  flex: '1 1 auto',
-  height: '100%',
-  overflow: 'auto'
-});
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { experimentalStyled } from "@material-ui/core";
+import DashboardNavbar from "./DashboardNavbar";
+import DashboardSidebar from "./DashboardSidebar";
 
 const DashboardLayout = () => {
   const [isMobileNavOpen, setMobileNavOpen] = useState(false);
   const [isDesktopNavOpen, setDesktopNavOpen] = useState(true);
 
+  const DashboardLayoutRoot = experimentalStyled("div")(({ theme }) => ({
+    backgroundColor: theme.palette.background.default,
+    display: "flex",
+    height: "100%",
+    overflow: "hidden",
+    width: "100%",
+  }));
+
+  const DashboardLayoutContainer = experimentalStyled("div")({
+    display: "flex",
+    flex: "1 1 auto",
+    overflow: "hidden",
+  });
+
+  const DashboardLayoutContent = experimentalStyled("div")({
+    flex: "1 1 auto",
+    height: "100%",
+    overflow: "auto",
+  });
+
+  const DashboardLayoutWrapper = experimentalStyled("div")(({ theme }) => ({
+    display: "flex",
+    flex: "1 1 auto",
+    overflow: "hidden",
+    paddingTop: 64,
+    [theme.breakpoints.up("lg")]: {
+      paddingRight: isDesktopNavOpen ? 256 : 0,
+    },
+  }));
+
   return (
     <DashboardLayoutRoot>
-      <DashboardNavbar onMobileNavOpen={() => setMobileNavOpen(true)} onDesktopNavOpen={() => setDesktopNavOpen(!isDesktopNavOpen)} />
+      <DashboardNavbar
+        onMobileNavOpen={() => setMobileNavOpen(true)}
+        onDesktopNavOpen={() => setDesktopNavOpen(!isDesktopNavOpen)}
+      />
       <DashboardSidebar
         onMobileClose={() => setMobileNavOpen(false)}
         openMobile={isMobileNavOpen}
         onDesktopClose={() => setDesktopNavOpen(true)}
         openDesktop={isDesktopNavOpen}
       />
-      <DashboardLayoutWrapper isDesktopNavOpen={isDesktopNavOpen}>
+      <DashboardLayoutWrapper>
         <DashboardLayoutContainer>
           <DashboardLayoutContent>
             <Outlet />
