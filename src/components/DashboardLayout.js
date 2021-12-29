fc72797631c5ceca4119620160af54fd6f14b897
LAPTOP-UUH0CQCK\Shamim <sh.jozi@medrick.info>
2021-12-29 18:33:13 +0330

    bug fix - menu bar responsive menu

--- src/components/DashboardLayout.js
+++ src/components/DashboardLayout.js
@@ -4,29 +4,29 @@ import { experimentalStyled } from "@material-ui/core";
 import DashboardNavbar from "./DashboardNavbar";
 import DashboardSidebar from "./DashboardSidebar";
 
+const DashboardLayoutRoot = experimentalStyled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.default,
+  display: "flex",
+  height: "100%",
+  overflow: "hidden",
+  width: "100%",
+}));
+
+const DashboardLayoutContainer = experimentalStyled("div")({
+  display: "flex",
+  flex: "1 1 auto",
+  overflow: "hidden",
+});
+
+const DashboardLayoutContent = experimentalStyled("div")({
+  flex: "1 1 auto",
+  height: "100%",
+  overflow: "auto",
+});
+
 const DashboardLayout = () => {
   const [isMobileNavOpen, setMobileNavOpen] = useState(false);
   const [isDesktopNavOpen, setDesktopNavOpen] = useState(true);
-
-  const DashboardLayoutRoot = experimentalStyled("div")(({ theme }) => ({
-    backgroundColor: theme.palette.background.default,
-    display: "flex",
-    height: "100%",
-    overflow: "hidden",
-    width: "100%",
-  }));
-
-  const DashboardLayoutContainer = experimentalStyled("div")({
-    display: "flex",
-    flex: "1 1 auto",
-    overflow: "hidden",
-  });
-
-  const DashboardLayoutContent = experimentalStyled("div")({
-    flex: "1 1 auto",
-    height: "100%",
-    overflow: "auto",
-  });
 
   const DashboardLayoutWrapper = experimentalStyled("div")(({ theme }) => ({
     display: "flex",
