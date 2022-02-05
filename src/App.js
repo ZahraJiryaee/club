a976d261f5121058d6f2d7c75242e40aef4c41da
Zahra Jiryaee <zahrajiryaee77@gmail.com>
2022-02-05 18:04:50 +0330

    add lucky wheel component sample - not completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import LuckyWheelPage from "./pages/lucky-wheel/lucky-wheel.component";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Routes>
+        <Route path="/lucky-wheel" element={<LuckyWheelPage />} />
+        <Route path="/" element={<Navigate replace to="/lucky-wheel" />} />
+      </Routes>
+    </Fragment>
   );
 }
 
---
+++ src/pages/lucky-wheel/lucky-wheel.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SpinningWheel from "../../components/spinning-wheel/spinning-wheel.component";
+
+const LuckyWheelPage = () => (
+  <div>
+    <h2>Lucky wheel page.</h2>
+    <SpinningWheel />
+  </div>
+);
+
+export default LuckyWheelPage;
