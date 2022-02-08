207be62076d8c40c6c16037c6384e4270638b270
Zahra Jiryaee <zahrajiryaee77@gmail.com>
2022-02-08 12:38:50 +0330

    extract a new file for routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React, { Fragment } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 
-import Footer from "./components/footer/footer.component";
-import Header from "./components/header/header.component";
-
-import LuckyWheelPage from "./pages/lucky-wheel/lucky-wheel.component";
+import routes from "./routes";
 
 function App() {
-  return (
-    <Fragment>
-      <Header />
-      <Routes>
-        <Route path="/lucky-wheel" element={<LuckyWheelPage />} />
-        <Route path="/profile" element={<LuckyWheelPage />} />
-        <Route path="/shop" element={<LuckyWheelPage />} />
-        <Route path="/games" element={<LuckyWheelPage />} />
-        <Route path="/leaderboard" element={<LuckyWheelPage />} />
-        <Route path="/home" element={<LuckyWheelPage />} />
-        <Route path="/about-us" element={<LuckyWheelPage />} />
-        <Route path="/contact-us" element={<LuckyWheelPage />} />
-        <Route path="/" element={<Navigate replace to="/lucky-wheel" />} />
-      </Routes>
-      <Footer />
-    </Fragment>
-  );
+  const routing = useRoutes(routes);
+  return <Fragment>{routing}</Fragment>;
 }
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+import MainLayout from "./layouts/main-layout/main-layout.component";
+
+import LuckyWheelPage from "./pages/lucky-wheel/lucky-wheel.component";
+
+const routes = [
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "lucky-wheel", element: <LuckyWheelPage /> },
+      { path: "profile", element: <LuckyWheelPage /> },
+      { path: "shop", element: <LuckyWheelPage /> },
+      { path: "games", element: <LuckyWheelPage /> },
+      { path: "leaderboard", element: <LuckyWheelPage /> },
+      { path: "home", element: <LuckyWheelPage /> },
+      { path: "about-us", element: <LuckyWheelPage /> },
+      { path: "contact-us", element: <LuckyWheelPage /> },
+      { path: "/", element: <Navigate replace to="/lucky-wheel" /> },
+      { path: "*", element: <Navigate replace to="/lucky-wheel" /> },
+    ],
+  },
+];
+
+export default routes;
