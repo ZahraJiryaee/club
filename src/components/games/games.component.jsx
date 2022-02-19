b098112b4c14af600010b4cdf22271b7f1bdebb3
LAPTOP-UUH0CQCK\Shamim <sh.jozi@medrick.info>
2022-02-19 16:08:55 +0330

    work on genre page

--- src/components/games/games.component.jsx
+++ src/components/games/games.component.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { getAllGenres } from "../../redux/genres/genres.action";
+import {
+  getAllGenres,
+  getSelectedGenre,
+} from "../../redux/genres/genres.action";
 
 import GenreHeader from "./genre-header.component";
 import RowGames from "./games.row.component";
 import BannerGames from "./games.banner.component";
+import GenrePage from "./games.genre.component";
 
 import "./games.styles.scss";
 
@@ -14,41 +18,45 @@ const Games = () => {
   const games = useSelector((state) => state.games.allGames);
 
   //Mock
-  let genre = "همه";
+  let genre = "هههمه";
+  let id = "c91cb401-1bec-4d0a-b77a-503b5a6c6fb9";
 
   useEffect(() => {
     dispatch(getAllGenres());
+    dispatch(getSelectedGenre(id));
   }, []);
 
   return (
     <div>
       <GenreHeader />
       <div className="games-page">
-        {genre === "همه"
-          ? games.map((category) => {
-              return category.enumerate_type === 1 ? (
-                <div key={category.id}>
-                  <div className="category-container">
-                    <span className="category-title">{category.title}</span>
-                    <div>
-                      <span className="more-button">بیشتر</span>
-                      <span className="more-icon">
-                        <i className="fa fa-solid fa-chevron-left" />
-                      </span>
-                    </div>
+        {genre === "همه" ? (
+          games.map((category) => {
+            return category.enumerate_type === 1 ? (
+              <div key={category.id}>
+                <div className="category-container">
+                  <span className="category-title">{category.title}</span>
+                  <div>
+                    <span className="more-button">بیشتر</span>
+                    <span className="more-icon">
+                      <i className="fa fa-solid fa-chevron-left" />
+                    </span>
                   </div>
-                  <RowGames category={category} />
                 </div>
-              ) : (
-                <div key={category.id}>
-                  <div className="category-container">
-                    <span className="category-title">{category.title}</span>
-                  </div>
-                  <BannerGames category={category} />
+                <RowGames category={category} />
+              </div>
+            ) : (
+              <div key={category.id}>
+                <div className="category-container">
+                  <span className="category-title">{category.title}</span>
                 </div>
-              );
-            })
-          : null}
+                <BannerGames category={category} />
+              </div>
+            );
+          })
+        ) : (
+          <GenrePage />
+        )}
       </div>
     </div>
   );
---
+++ src/components/games/games.genre.component.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { ReactComponent as StarLogo } from "../../assets/images/icon/star.svg";
+
+import "./games.genre.styles.scss";
+
+const GenrePage = () => {
+  const filteredGames = useSelector((state) => state.genres.filteredGenre);
+
+  return (
+    <div>
+      {filteredGames.map((application) => {
+        return (
+          <div className="genre-container" key={application.id}>
+            <div className="application-container">
+              <div className="application-details">
+                <img className="icon" src={application.source.icon} />
+                <div className="description">
+                  <span className="title">{application.name}</span>
+                  <span className="install-score">{`${application.install_score_counter} امتیاز با نصب این بازی دریافت کنید.`}</span>
+                </div>
+              </div>
+              <div className="application-rate">
+                <br />
+                <div>
+                  <span className="rate-text">{application.rate}</span>
+                </div>
+                <StarLogo className="rate-icon" />
+              </div>
+            </div>
+            <div className="banner-container">
+              {application.source.screenshots.map((screenshot, index) => {
+                return <img className="banner" key={index} src={screenshot} />;
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default GenrePage;
