2709c3e3179b90ffd7153b79978f6c985346b15e
LAPTOP-UUH0CQCK\Shamim <sh.jozi@medrick.info>
2022-02-19 12:36:54 +0330

    delete .vscode

--- src/components/games/games.component.jsx
+++ src/components/games/games.component.jsx
@@ -12,7 +12,6 @@ import "./games.styles.scss";
 const Games = () => {
   const dispatch = useDispatch();
   const games = useSelector((state) => state.games.allGames);
-  games.sort((a, b) => (a.priority < b.priority ? -1 : 1));
 
   //Mock
   let genre = "همه";
