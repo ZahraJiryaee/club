8e6435741410638c70e7250972f6b961f6cc7ff1
z.jiryaee <zahrajiryaee@gmail.com>
2022-04-18 17:32:39 +0430

    shop page responsive design

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,6 +4,8 @@ import ShopHeader from "../../components/shop-header/shop-header.component";
 
 import shopMock from "../../components/mock/shop.mock";
 
+import ShopItem from "./../../assets/images/test/shop-item.png";
+
 import "./shop.styles.scss";
 
 const ShopPage = () => (
@@ -16,14 +18,17 @@ const ShopPage = () => (
         (item, index) =>
           index < 12 && (
             <div className="shop-item" key={item.id}>
-              <img
-                className="shop-item-img"
-                src="https://th.bing.com/th/id/OIP.MPz376vD6rfBlfArgX-6GQAAAA?w=181&h=181&c=7&r=0&o=5&pid=1.7"
-                alt="shop-item-img"
-              />
-              <div className="shop-item-name">{item.name}</div>
-              <div className="shop-item-score-needed">
-                {item.scoreNeeded} امتیاز
+              <div className="shop-item-img-container">
+                <div
+                  className="shop-item-img"
+                  style={{ backgroundImage: `url(${ShopItem})` }}
+                ></div>
+              </div>
+              <div className="name-score-container">
+                <div className="shop-item-name">{item.name}</div>
+                <div className="shop-item-score-needed">
+                  {item.scoreNeeded} امتیاز
+                </div>
               </div>
               <div className="shop-item-purchase-btn">خرید</div>
             </div>
