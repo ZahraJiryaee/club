f5a94bde76eb0d836233616eea57f72f6ba2d0d7
LAPTOP-UUH0CQCK\Shamim <sh.jozi@medrick.info>
2022-04-23 17:40:34 +0430

    handle header mode

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router";
+import { useDispatch } from "react-redux";
+
+import { setHeaderMode } from "../../redux/header/header.action";
 
 import ShopHeader from "../../components/shop-header/shop-header.component";
 
@@ -8,34 +12,43 @@ import ShopItem from "./../../assets/images/test/shop-item.png";
 
 import "./shop.styles.scss";
 
-const ShopPage = () => (
-  <div>
-    {/* header */}
-    <ShopHeader />
-    {/* content */}
-    <span className="shop-grid-data">
-      {shopMock.shopItems.map(
-        (item, index) =>
-          index < 12 && (
-            <div className="shop-item" key={item.id}>
-              <div className="shop-item-img-container">
-                <div
-                  className="shop-item-img"
-                  style={{ backgroundImage: `url(${ShopItem})` }}
-                ></div>
-              </div>
-              <div className="name-score-container">
-                <div className="shop-item-name">{item.name}</div>
-                <div className="shop-item-score-needed">
-                  {item.scoreNeeded} امتیاز
+const ShopPage = () => {
+  const { pathname } = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setHeaderMode(pathname));
+  }, []);
+
+  return (
+    <div>
+      {/* header */}
+      <ShopHeader />
+      {/* content */}
+      <span className="shop-grid-data">
+        {shopMock.shopItems.map(
+          (item, index) =>
+            index < 12 && (
+              <div className="shop-item" key={item.id}>
+                <div className="shop-item-img-container">
+                  <div
+                    className="shop-item-img"
+                    style={{ backgroundImage: `url(${ShopItem})` }}
+                  ></div>
                 </div>
+                <div className="name-score-container">
+                  <div className="shop-item-name">{item.name}</div>
+                  <div className="shop-item-score-needed">
+                    {item.scoreNeeded} امتیاز
+                  </div>
+                </div>
+                <div className="shop-item-purchase-btn">خرید</div>
               </div>
-              <div className="shop-item-purchase-btn">خرید</div>
-            </div>
-          )
-      )}
-    </span>
-  </div>
-);
+            )
+        )}
+      </span>
+    </div>
+  );
+};
 
 export default ShopPage;
