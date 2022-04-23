20563d60cf549dd86f39c11014812fed94dcf688
LAPTOP-UUH0CQCK\Shamim <sh.jozi@medrick.info>
2022-04-23 16:53:04 +0430

    Add Loading component

--- src/pages/genre/genre.component.jsx
+++ src/pages/genre/genre.component.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
 
 import { getSelectedGenre } from "../../redux/genres/genres.action";
 import { getSelectedCategory } from "../../redux/games/games.action";
+
+import Loading from "../../components/common/Loading.component";
 
 import GenreHeader from "../../components/genres/genre-header.component";
 import GenreView from "../../components/genres/genre.view.component";
@@ -14,22 +16,46 @@ const GenrePage = () => {
   const allCategories = useSelector((state) => state.games.allGames);
   const [categoryTitle, setCategoryTitle] = useState("");
 
-  useEffect(() => {
-    if (type === "genre") {
-      dispatch(getSelectedGenre(id));
-    } else if (type === "category") {
-      dispatch(getSelectedCategory(id));
-      for (let i = 0; i < allCategories.length; i++) {
-        if (allCategories[i]["id"] === id)
-          setCategoryTitle(allCategories[i]["title"]);
+  const [loading, setLoading] = useState(true);
+
+  const setGenres = useCallback(async () => {
+    try {
+      setLoading(true);
+      if (type === "genre") {
+        await dispatch(getSelectedGenre(id));
+      } else if (type === "category") {
+        await dispatch(getSelectedCategory(id));
+        for (let i = 0; i < allCategories.length; i++) {
+          if (allCategories[i]["id"] === id)
+            setCategoryTitle(allCategories[i]["title"]);
+        }
       }
+    } catch (ex) {
+      console.log(ex);
+      //tast error
+    } finally {
+      setLoading(false);
     }
-  });
+  }, [type, id, allCategories]);
+
+  useEffect(() => {
+    setGenres();
+  }, [setGenres]);
 
   return (
     <div>
       <GenreHeader />
-      <GenreView title={categoryTitle} />
+      {loading ? (
+        <Loading
+          type="spinningBubbles"
+          color="#3399de"
+          height="50px"
+          width="50px"
+          text="...لطفا منتظر بمانید"
+        />
+      ) : (
+        <GenreView title={categoryTitle} />
+      )}
     </div>
   );
 };
