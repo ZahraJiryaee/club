10a5d27ec6aadb93cb54546329f0d1c2b45d5fd3
LAPTOP-UUH0CQCK\Shamim <sh.jozi@medrick.info>
2022-04-26 00:29:14 +0430

    set new access and refresh token

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { setNewToken } from "./userServices";
+
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 const token = localStorage.getItem("token");
@@ -11,6 +13,19 @@ axios.interceptors.response.use(null, (error) => {
     error.response &&
     error.response.status >= 400 &&
     error.response.status < 500;
+  if (error.response.status === 401) {
+    const refreshToken = localStorage.getItem("refreshToken");
+    const { status, data } = setNewToken(refreshToken);
+    if (status === 200) {
+      //toast -> try again...
+      localStorage.setItem("accessToken", data.access);
+      localStorage.setItem("refreshToken", data.refresh);
+    } else {
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+      //Go to login popup
+    }
+  }
   if (!expectedErrors) {
     console.log("Server error!");
     console.log(error);
