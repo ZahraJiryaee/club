5a769c443012d983d634cc7f59d66e79fcbe5048
LAPTOP-UUH0CQCK\Shamim <sh.jozi@medrick.info>
2022-04-30 15:58:05 +0430

    resolve conflicts

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+import { error } from "./toastService";
 import { setNewToken } from "./userServices";
 
 axios.defaults.headers.post["Content-Type"] = "application/json";
@@ -13,7 +14,17 @@ axios.interceptors.response.use(null, (error) => {
     error.response &&
     error.response.status >= 400 &&
     error.response.status < 500;
-  if (error.response.status === 401) {
+  if (!expectedErrors) {
+    error("Server error!");
+    console.log(error);
+  } else {
+    errorHandling(error.response.status);
+  }
+  return Promise.reject(error);
+});
+
+const errorHandling = (status) => {
+  if (status === 401) {
     const refreshToken = localStorage.getItem("refreshToken");
     const { status, data } = setNewToken(refreshToken);
     if (status === 200) {
@@ -23,15 +34,17 @@ axios.interceptors.response.use(null, (error) => {
     } else {
       localStorage.removeItem("accessToken");
       localStorage.removeItem("refreshToken");
+      return error("User Unauthorized");
       //Go to login popup
     }
+  } else if (status === 403) {
+    return error("Access_denied");
+  } else if (status === 404) {
+    return error("یافت نشد");
+  } else {
+    return error("Something went wrong! Please try again.");
   }
-  if (!expectedErrors) {
-    console.log("Server error!");
-    console.log(error);
-  }
-  return Promise.reject(error);
-});
+};
 
 export default {
   get: axios.get,
