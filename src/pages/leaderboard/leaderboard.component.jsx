4b188ed58d691b325c6f8089d7b41035e5563bfd
Zahra Jiryaee <zahrajiryaee77@gmail.com>
2022-05-17 18:41:41 +0430

    leaderboard; apply styles to the new logic - clenup

--- src/pages/leaderboard/leaderboard.component.jsx
+++ src/pages/leaderboard/leaderboard.component.jsx
@@ -20,7 +20,7 @@ const LeaderBoardPage = () => {
 
   const [crtUser, setCrtUser] = useState({
     id: "tg35",
-    index: 4,
+    index: 5,
     name: "علی حاتیاننننصثیم",
     score: 111,
     prize: "جایزه",
@@ -73,6 +73,7 @@ const LeaderBoardPage = () => {
 
       {/* content */}
       <div className="leaderboard-container">
+        <p className="leaderborad-headline-txt">جدول رده‌بندی</p>
         <div className="center-flex">
           <div
             className={`leaderboard-user-list ${
@@ -82,70 +83,54 @@ const LeaderBoardPage = () => {
             }`}
           >
             {topUsers
-              .filter((item, idx) => idx <= 13)
+              .filter((user, index) => index <= 13)
               .map((user, index) => (
                 <div
                   className={`leaderboard-each-user ${
-                    crtUser.index === index ? "current-user-in-first-14" : ""
+                    crtUser.index === index
+                      ? "current-user-in-first-14 current-user-loggedin"
+                      : ""
                   } ${
                     checkIFUserIsInBetween(index)
-                      ? "current-user-between-7-and-13"
+                      ? "current-user-between-7-and-13 current-user-loggedin"
                       : ""
                   }`}
                 >
-                  item-{index + 1}
+                  <UserRecord user={user} index={index} />
                 </div>
               ))}
           </div>
         </div>
         <div className="center-flex">
           {crtUser.index >= 14 && (
-            <div className="current-user-out-first-14">
-              item---{crtUser.index + 1}
+            <div className="current-user-out-first-14 current-user-loggedin">
+              <UserRecord user={crtUser} index={crtUser.index} />
             </div>
           )}
         </div>
       </div>
-
-      {/*  
-      <div className="leaderboard-container">
-        <p className="leaderborad-topusers-grid-txt">جدول رده‌بندی</p>
-        <div className="leaderborad-topusers-grid">
-          {topUsers.map(
-            (user, index) =>
-              (index < 14 || user.user) && (
-                <div
-                  key={index}
-                  className={`each-user ${user.user ? "loggedin-user" : ""} ${
-                    user.user && index > 14 ? "loggedin-user-in-middle" : ""
-                  }`}
-                >
-                  <span className="user-index">{index + 1}</span>
-                  <div className="user-record">
-                    <div className="user--avatar-name">
-                      <img
-                        src="https://cdn3.iconfinder.com/data/icons/avatars-round-flat/33/avat-01-512.png"
-                        alt="user-avater"
-                        className="avatar"
-                      />
-                      <span className="name-score">
-                        <div>{user.name}</div>
-                      </span>
-                    </div>
-                    <div className="user--score-prize">
-                      {user.prize && <span>جایزه</span>}
-                      <span>{user.score} امتیاز</span>
-                    </div>
-                  </div>
-                </div>
-              )
-          )}
-        </div>
-        <p>hi</p>
-      </div>
-      */}
     </div>
   );
 };
+
+const UserRecord = ({ user, index }) => (
+  <div className="user-record">
+    <span className="user-index">{index + 1}</span>
+    <div className="user-info">
+      <div className="user--avatar-name">
+        <img
+          src="https://cdn3.iconfinder.com/data/icons/avatars-round-flat/33/avat-01-512.png"
+          alt="user-avater"
+          className="avatar"
+        />
+        <span className="name">{user.name}</span>
+      </div>
+      <div className="user--score-prize">
+        {user.prize && <span>جایزه</span>}
+        <span>{user.score} امتیاز</span>
+      </div>
+    </div>
+  </div>
+);
 
 export default LeaderBoardPage;
