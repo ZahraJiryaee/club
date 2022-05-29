36b378c931fe9d3400f8cc010a850256aa3e3e74
Zahra Jiryaee <zahrajiryaee77@gmail.com>
2022-05-29 20:09:42 +0430

    handle shop search on client side - enhance search with submit button

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -11,16 +11,24 @@ const SearchBox = ({
   return (
     <div className="search-box-container">
       <div className="search-box">
-        <div onClick={onSearchIconClick}>
-          <i className="fa fa-search search-icon" />
-        </div>
-        <input
-          value={searchField}
-          className="search-text"
-          type="text"
-          placeholder={searchInputPlaceHolder}
-          onChange={(e) => setSearchField(e.target.value)}
-        />
+        <form
+          className="search-box-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            return onSearchIconClick();
+          }}
+        >
+          <button type="submit" className="search-icon-container">
+            <i className="fa fa-search search-icon" />
+          </button>
+          <input
+            value={searchField}
+            className="search-text"
+            type="text"
+            placeholder={searchInputPlaceHolder}
+            onChange={(e) => setSearchField(e.target.value)}
+          />
+        </form>
       </div>
     </div>
   );
--- src/components/shop-header/shop-header.component.jsx
+++ src/components/shop-header/shop-header.component.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 import { getSearchedShopItems } from "../../redux/shop/shop.actions";
+
+import { selectAllShopItems } from "../../redux/shop/shop.selectors";
 
 import ComponentInternalHeader from "../compoonent-internal-header/compoonent-internal-header.component";
 import SearchBox from "../search-box/search-box.component";
 
 import { routeNames } from "../../services/routeService";
-
-import shopMock from "../../components/mock/shop.mock";
 
 import "./shop-header.styles.scss";
 
@@ -17,11 +17,17 @@ const ShopHeader = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const allShopItems = useSelector(selectAllShopItems);
+
   const [searchField, setSearchField] = React.useState("");
 
   const handelShopSearch = async () => {
     setSearchField("");
-    dispatch(getSearchedShopItems(shopMock.shopItems));
+    let searchedItem = [];
+    allShopItems.forEach((item) => {
+      if (item.title.includes(searchField)) searchedItem.push(item);
+    });
+    dispatch(getSearchedShopItems(searchedItem));
     return navigate(`/${routeNames.shop}/search/${searchField}`);
   };
 
