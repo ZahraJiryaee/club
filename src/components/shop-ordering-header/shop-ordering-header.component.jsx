1443137673e82144a32ed8ac750ec837f37c4ece
z.jiryaee <zahrajiryaee77@gmail.com>
2022-05-30 19:44:09 +0430

    divide shop header into 2 components, use pagination in shop

---
+++ src/components/shop-ordering-header/shop-ordering-header.component.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+
+import shopMock from "../mock/shop.mock";
+
+import "./shop-ordering-header.styles.scss";
+
+const ShopOrderingHeader = () => {
+  const { t } = useTranslation();
+
+  const [toggleSelect, setToggleSelect] = useState(false);
+  const [orderShop, setOrderShop] = useState(shopMock.orderShop);
+  const handleSelectToggle = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setToggleSelect(!toggleSelect);
+  };
+  const handleCheckedItem = (id) => {
+    /* set the "checked" value for the selected id to true & others to false */
+    console.log("id:", id);
+    if (toggleSelect) {
+      let clone = [...orderShop];
+      let picked_data = clone.filter((item) => item.id === id);
+      for (let i = 0; i < clone.length; i++) {
+        if (clone[i].id === picked_data[0].id) {
+          clone[i].checked = true;
+        } else clone[i].checked = false;
+      }
+      setOrderShop(clone);
+    }
+  };
+
+  return (
+    <div className="shop-header-container">
+      <div className="shop-header">
+        <span className="txt">{t("Sorting")}:</span>
+        <span
+          className={`dropdown ${toggleSelect ? "expanded" : ""}`}
+          onClick={(e) => handleSelectToggle(e)}
+        >
+          {orderShop.map((item) => (
+            <React.Fragment key={item.id}>
+              <input
+                type="radio"
+                name={`sort-${item.content}`}
+                id={`sort-${item.content}`}
+                value={item.content}
+                checked={item.checked}
+                readOnly
+              />
+              <label
+                htmlFor={`sort-${item.content}`}
+                onClick={() => handleCheckedItem(item.id)}
+              >
+                {item.content}
+              </label>
+            </React.Fragment>
+          ))}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default ShopOrderingHeader;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,52 +1,96 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { useLocation } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
 
 import { setHeaderMode } from "../../redux/header/header.action";
+import {
+  setOpenShopModal,
+  setShopModalData,
+  getAllShopItems,
+} from "../../redux/shop/shop.actions";
 
+import { selectAllShopItems } from "../../redux/shop/shop.selectors";
+
+import ShopOrderingHeader from "../../components/shop-ordering-header/shop-ordering-header.component";
 import ShopHeader from "../../components/shop-header/shop-header.component";
+import Pagination from "../../components/common/pagination/pagination.component";
 
-import shopMock from "../../components/mock/shop.mock";
+// import shopMock from "../../components/mock/shop.mock";
 
 import ShopItem from "./../../assets/images/test/shop-item.png";
 
 import "./shop.styles.scss";
 
+let PageSize = 4 * 4;
+
 const ShopPage = () => {
   const { pathname } = useLocation();
   const dispatch = useDispatch();
+  const { t } = useTranslation();
+
+  // const allShopItems = shopMock.shopItems;
+  const allShopItems = useSelector(selectAllShopItems);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const currentShopData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return allShopItems.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
+  const onPurchaseClick = (item) => {
+    dispatch(setOpenShopModal(true));
+    dispatch(setShopModalData(item));
+  };
 
   useEffect(() => {
     dispatch(setHeaderMode(pathname));
   }, []);
 
+  useEffect(() => {
+    dispatch(getAllShopItems());
+  }, []);
+
   return (
     <div>
-      {/* header */}
+      {/* General Haeder */}
       <ShopHeader />
+
+      {/* Ordering header */}
+      <ShopOrderingHeader />
+
       {/* content */}
       <span className="shop-grid-data">
-        {shopMock.shopItems.map(
-          (item, index) =>
-            index < 12 && (
-              <div className="shop-item" key={item.id}>
-                <div className="shop-item-img-container">
-                  <div
-                    className="shop-item-img"
-                    style={{ backgroundImage: `url(${ShopItem})` }}
-                  ></div>
-                </div>
-                <div className="name-score-container">
-                  <div className="shop-item-name">{item.name}</div>
-                  <div className="shop-item-score-needed">
-                    {item.scoreNeeded} امتیاز
-                  </div>
-                </div>
-                <div className="shop-item-purchase-btn">خرید</div>
+        {currentShopData.map((item) => (
+          <div className="shop-item" key={item.id}>
+            <div className="shop-item-img-container">
+              <img className="shop-item-img" src={ShopItem} alt="shop-item" />
+            </div>
+            <div className="name-score-container">
+              <div className="shop-item-name">{item.title}</div>
+              <div className="shop-item-score-needed">
+                {item.leave_chance_counter} {t("Score")}
               </div>
-            )
-        )}
+            </div>
+            <button
+              className="shop-item-purchase-btn-position shop-item-purchase-btn"
+              onClick={() => onPurchaseClick(item)}
+            >
+              {t("Purchase")}
+            </button>
+          </div>
+        ))}
       </span>
+
+      {/* Pagination */}
+      <Pagination
+        currentPage={currentPage}
+        totalCount={allShopItems.length}
+        pageSize={PageSize}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
     </div>
   );
 };
