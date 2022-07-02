528c27fef110dcb5ce9e9031a833031c302c859d
z.jiryaee <zahrajiryaee77@gmail.com>
2022-07-02 12:35:56 +0430

    login component ; validation fields

--- src/components/signup-signin/signin/signin.component.jsx
+++ src/components/signup-signin/signin/signin.component.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import { useTranslation } from "react-i18next";
 
 import { login } from "../../../redux/user/user.action";
 
+import CustomInput from "../../common/custom-input/custom-input.component";
+import CustomButton from "../../common/custom-button/custom-button.component";
+
+import { validateMobileNumber } from "../../../services/validateMobileNumber";
 import logger from "../../../services/logService";
 
 const Signin = ({ setSignupMode }) => {
   const dispatch = useDispatch();
+  const { t } = useTranslation();
+
+  const mobilNumberMaxLength = 11;
+  const passwordMinLength = 8;
 
   const [passwordVisible, setPasswordVisible] = useState(false);
 
   const [signinMobileNumber, setSigninMobileNumber] = useState("");
   const [signinPassword, setSigninPassword] = useState("");
+
+  const [isSigninMobileNumberValid, setIsSigninMobileNumberValid] =
+    useState(false);
+  const [mobileNumberNotValidErrorMsg, setMobileNumberNotValidErrorMsg] =
+    useState("");
+  const [passwordNotValidErrorMsg, setPasswordNotValidErrorMsg] = useState("");
 
   const [disableSubmitButton, setDisableSubmitButton] = useState(true);
 
@@ -33,71 +48,66 @@ const Signin = ({ setSignupMode }) => {
     const { value, maxLength } = event.target;
     const mobileNumber = value.slice(0, maxLength);
     setSigninMobileNumber(mobileNumber);
-    setDisableSubmitButton(
-      signinMobileNumber.length === 11 && signinPassword.length >= 8
-        ? false
-        : true
-    );
+
+    if (validateMobileNumber(mobileNumber)) {
+      setIsSigninMobileNumberValid(true);
+      setMobileNumberNotValidErrorMsg(null);
+    } else {
+      setIsSigninMobileNumberValid(false);
+      setMobileNumberNotValidErrorMsg(t("Mobile_Number_Not_valid_Error_Msg"));
+    }
   };
 
   const handleSigninPasswordChange = (event) => {
     const { value, minLength } = event.target;
-    const password = value;
-    setSigninPassword(password);
+    setSigninPassword(value);
+
+    setPasswordNotValidErrorMsg(
+      value.length < minLength
+        ? t("Password_Should_Contain_Atleast_8_Chars")
+        : null
+    );
+  };
+
+  useEffect(() => {
     setDisableSubmitButton(
-      signinMobileNumber.length === 11 && signinPassword.length >= minLength
+      isSigninMobileNumberValid && signinPassword.length >= passwordMinLength
         ? false
         : true
     );
-  };
+  }, [isSigninMobileNumberValid, passwordMinLength, signinPassword]);
 
   return (
     <React.Fragment>
       <span className="text-right bolder-txt space-padding--up--30">ورود</span>
-      {/* username */}
-      <span className="text-right lighter-txt space-padding--up--40">
-        نام کاربری <span className="compulsory-txt">(اجباری)</span>
-      </span>
-      <div className="signup-signin-input-container">
-        <input
-          className="signup-signin-input space-margin--up--20"
-          type="number"
-          maxLength={11}
-          placeholder="مثلا ۰۹۱۲۱۲۳۴۵۶۷"
-          value={signinMobileNumber}
-          onChange={(e) => handleSigninMobileNumberChange(e)}
-        />
-      </div>
-      {/* password */}
-      <span className="text-right lighter-txt space-padding--up--45">
-        کلمه عبور <span className="compulsory-txt">(اجباری)</span>
-      </span>
-      <div className="signup-signin-input-container">
-        <input
-          className="signup-signin-input space-margin--up--20"
-          type={`${passwordVisible ? "text" : "password"}`}
-          minLength="8"
-          value={signinPassword}
-          onChange={(e) => handleSigninPasswordChange(e)}
-        />
-        <i
-          className={`fa fa-eye${
-            !passwordVisible ? "-slash" : ""
-          } input-eye-icon`}
-          aria-hidden="true"
-          onClick={handlePasswordVisibleIconToggle}
-        ></i>
-      </div>
-
-      <button
-        className={`btn space-margin--up--65 ${
-          disableSubmitButton ? "disabled" : ""
-        }`}
+      <CustomInput
+        icon="mobile"
+        label="شماره موبایل"
+        type="number"
+        maxLength={mobilNumberMaxLength}
+        placeholder="مثلا ۰۹۱۲۱۲۳۴۵۶۷"
+        value={signinMobileNumber}
+        onValueChange={handleSigninMobileNumberChange}
+        errorMsg={mobileNumberNotValidErrorMsg}
+      />
+      <CustomInput
+        icon={`eye${!passwordVisible ? "-slash" : ""}`}
+        label="کلمه عبور"
+        type={`${passwordVisible ? "text" : "password"}`}
+        minLength={passwordMinLength}
+        placeholder=""
+        value={signinPassword}
+        onValueChange={handleSigninPasswordChange}
+        onPassword1VisibleIconToggle={handlePasswordVisibleIconToggle}
+        errorMsg={passwordNotValidErrorMsg}
+      />
+      <CustomButton
+        btnBgColor="marine-blue"
         onClick={handleSignin}
         disabled={disableSubmitButton}
       >
         ورود
-      </button>
+      </CustomButton>
       <p className="switch-between-signin-signup-text">
         اگر در مدریک کلاب حساب کاربری ندارید،
         <span className="link" onClick={() => setSignupMode(true)}>
