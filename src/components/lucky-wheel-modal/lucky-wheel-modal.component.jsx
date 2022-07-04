825e2e153d57f2b13a24dab011e1635d66ec3709
z.jiryaee <zahrajiryaee77@gmail.com>
2022-07-04 20:07:30 +0430

    rewrite lucky-wheel-modal ; digital bonuses

--- src/components/lucky-wheel-modal/lucky-wheel-modal.component.jsx
+++ src/components/lucky-wheel-modal/lucky-wheel-modal.component.jsx
@@ -18,7 +18,6 @@ import { selectAllUserAddresses } from "../../redux/user/user.selectors";
 import CustomRadioInput from "../common/custom-radio-input/custom-radio-input.component";
 import CustomButton from "../common/custom-button/custom-button.component";
 
-import logger from "../../services/logService";
 import generateUniqueId from "../../services/generateUniqueId";
 
 import CloseIcon from "./../../assets/images/icon/close-icon.png";
@@ -30,18 +29,6 @@ const LuckyWheelModal = () => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
 
-  const mockData = {
-    addresses: [
-      "آدرس  dskjcfb soicj ci cicddicd cduch cucg cdgc cygd cycgayscg aycga scasygc asycgasyc asycas cysgcysc ysysa cfuyas cayst1",
-      "آدرس 2",
-      "آدرس 3",
-      "آدرس 4",
-    ],
-  };
-  // const mockData = {
-  //   addresses: [],
-  // };
-
   const { detail: setBonus, log_id } = useSelector(selectBonus) || {};
   const allUserAddresses = useSelector(selectAllUserAddresses);
   const openWheelModal = useSelector(selectOpenWheelModal);
@@ -49,19 +36,23 @@ const LuckyWheelModal = () => {
   const { title, type: prizeType, code } = setBonus || {};
 
   const [luckyWheelModalStage, setLuckyWheelModalStage] = useState(1);
-  const [prizeReceived, setPrizeReceived] = useState(false);
   const [userAddress, setUserAddress] = useState("");
-  const [popupBtnTxt, setPopupBtnTxt] = useState(
-    prizeType === "dig" ? t("Receive") : t("Confirm")
-  );
   const [addressData, setAddressData] = useState([]);
+  const [addressSelected, setAddressSelected] = useState(null);
 
   useEffect(() => {
-    dispatch(getUserAddresses());
-  }, [dispatch]);
+    if (prizeType === "non-dig") {
+      dispatch(getUserAddresses());
+    }
+  }, [dispatch, setBonus, prizeType]);
 
   useEffect(() => {
-    console.log("allUserAddresses:", allUserAddresses);
+    setLuckyWheelModalStage(1);
+    setUserAddress("");
+    setAddressSelected(null);
+  }, [setBonus]);
+
+  useEffect(() => {
     if (prizeType === "non-dig") {
       if (allUserAddresses.addresses.length === 0) {
         setLuckyWheelModalStage(2);
@@ -74,15 +65,6 @@ const LuckyWheelModal = () => {
       }
     }
   }, [allUserAddresses]);
-
-  const popupPrizeBtn = () => {
-    setPrizeReceived(true);
-    if (prizeType === "dig") {
-      setPopupBtnTxt(`${t("Verification_Code")}: ${code}`);
-    } else if (prizeType === "non-dig") {
-      setPopupBtnTxt(`${t("Tracking_Code")}: ${code}`);
-    }
-  };
 
   const setBonusAddress = () => {
     const setBonusAddressBody = {
@@ -101,29 +83,11 @@ const LuckyWheelModal = () => {
     dispatch(setCurrentUser());
   };
 
-  useEffect(() => {
-    logger.logInfo("setBonus-wheel-modal:", setBonus);
-    const { type } = setBonus || {};
-
-    setPrizeReceived(false);
-    setUserAddress("");
-    setPopupBtnTxt(type === "dig" ? t("Receive") : t("Confirm"));
-  }, [setBonus, t]);
-
-  useEffect(() => {}, []);
-
-  const ModalTitle = (prizeType, luckyWheelModalStage) => {
-    return (
-      <p
-        className={`modal-title modal-title__${prizeType}-${luckyWheelModalStage}`}
-      >
-        {title}
-      </p>
-    );
+  const ModalTitle = () => {
+    return <p className="modal-title">{title}</p>;
   };
 
   /* ----------------------- NON DIGITAL BONUS -------------------------- */
-  const [addressSelected, setAddressSelected] = useState(null);
 
   const handleAddNewAddressClick = () => {
     setUserAddress("");
@@ -147,21 +111,20 @@ const LuckyWheelModal = () => {
   const handleAddressSelected = (value) => {
     setAddressSelected(value);
     const res = addressData.find((item) => item.id === value);
-    console.log("res:", res);
     setUserAddress(res.content);
   };
 
   const ChooseAddress = () => {
     return (
       <div className="center-absolute top--55 width-100">
-        {ModalTitle(prizeType, luckyWheelModalStage)}
-        <p className="modal-choose-address">{t("Choose_Address")}</p>
+        {ModalTitle()}
+        <p className="choose-address">{t("Choose_Address")}</p>
         <CustomRadioInput
           data={addressData}
           addressSelected={addressSelected}
           onAddressSelectedChange={handleAddressSelected}
         />
-        <p className="modal-add-new-address" onClick={handleAddNewAddressClick}>
+        <p className="add-new-address" onClick={handleAddNewAddressClick}>
           <img src={AddNewAddress} alt="" className="img" />
           {t("Add_New_Address")}
         </p>
@@ -179,7 +142,7 @@ const LuckyWheelModal = () => {
   const EnterNewAddress = () => {
     return (
       <div className="center-absolute top--55 dir-rtl width-100">
-        {ModalTitle(prizeType, luckyWheelModalStage)}
+        {ModalTitle()}
         <p className="user-address-label">
           <span>{t("Enter_Address")}</span>
           <span>{t("Required")}</span>
@@ -204,7 +167,7 @@ const LuckyWheelModal = () => {
   const SendToUserAddress = () => {
     return (
       <div className="center-absolute top--55">
-        {ModalTitle(prizeType, luckyWheelModalStage)}
+        {ModalTitle()}
         <p className="send-to-user-address">
           <span>{t("Send_To")}</span>
           <span>{userAddress}</span>
@@ -223,17 +186,53 @@ const LuckyWheelModal = () => {
     );
   };
 
+  /* ----------------------- DIGITAL BONUS -------------------------- */
+  const handleModalBtn_Phase1_Dig = () => {
+    setLuckyWheelModalStage(2);
+  };
+
+  const ShowDigitalBonusReceive = () => {
+    return (
+      <>
+        <div className="center-absolute top--50">{ModalTitle()}</div>
+
+        <div className="center-absolute top--75">
+          <CustomButton
+            btnBgColor="marine-blue"
+            onClick={handleModalBtn_Phase1_Dig}
+          >
+            {t("Receive")}
+          </CustomButton>
+        </div>
+      </>
+    );
+  };
+
+  const ShowDigitalBonusVerification = () => {
+    return (
+      <>
+        <div className="center-absolute top--45">{ModalTitle()}</div>
+        <div className="center-absolute top--65">
+          <CustomButton btnBgColor="deep-sky-blue">
+            {t("Verification_Code")}: {code}
+          </CustomButton>
+        </div>
+        <p className="center-absolute top--80 dig-prize-received-hint">
+          {t("Enter_Code_In_Game__LuckyWheelModal")}
+        </p>
+      </>
+    );
+  };
+
+  /* ----------------------- POPULATE SCREEN -------------------------- */
   const populateScreen = () => {
-    console.log("luckyWheelModalStage:", luckyWheelModalStage);
     switch (prizeType) {
       case "dig":
         switch (luckyWheelModalStage) {
           case 1:
-            break;
-
+            return <>{ShowDigitalBonusReceive()}</>;
           case 2:
-            break;
-
+            return <>{ShowDigitalBonusVerification()}</>;
           default:
             break;
         }
@@ -256,10 +255,10 @@ const LuckyWheelModal = () => {
     }
   };
 
-  return setBonus ? (
+  return openWheelModal && setBonus ? (
     <>
-      <div className="overlay">
-        <div className="popup">
+      <div className="lucky-wheel-modal-overlay">
+        <div className="lucky-wheel-modal-popup">
           <div
             className={`bg-hooray bg-hooray__${prizeType}_${luckyWheelModalStage}`}
           ></div>
@@ -272,66 +271,6 @@ const LuckyWheelModal = () => {
           />
           {/*  */}
           {populateScreen()}
-          {/*--------------------- title -------------------*/}
-
-          {/* <p
-            className={`popup-title popup-title-${prizeType}
-              popup-title-${prizeType}-prize-received-${prizeReceived}
-              `}
-          >
-            {title}
-          </p> */}
-          {/*--------------------- Non Digital Specific -------------------*/}
-
-          {/* {prizeType === "non-dig" && (
-            <div className="user-address-container">
-              {!prizeReceived ? (
-                <>
-                  <p className="user-address-label">
-                    <span>{t("Enter_Address")}</span>
-                    <span>{t("Required")}</span>
-                  </p>
-                  <textarea
-                    defaultValue={userAddress}
-                    className="user-address"
-                    onChange={handleTxt}
-                    autoFocus
-                  />
-                </>
-              ) : (
-                <>
-                  <p className="send-to-user-address">
-                    <span>{t("Send_To")}</span>
-                    <span>{userAddress}</span>
-                  </p>
-                  <p className="support-number">
-                    <span>{t("Support_Number")}</span>
-                    <span>{process.env.REACT_APP_SUPPORT_NUMBER}</span>
-                  </p>
-                </>
-              )}
-            </div>
-          )} */}
-          {/*--------------------- Digital Specific -------------------*/}
-          {/* {prizeType === "dig" && prizeReceived && (
-            <p className="center-absolute dig-prize-received-hint">
-              {t("Enter_Code_In_Game__LuckyWheelModal")}
-            </p>
-          )} */}
-          {/*--------------------- Button -------------------*/}
-          {/* <button
-            disabled={
-              (prizeType === "non-dig" && userAddress === "") || prizeReceived
-            }
-            className={`center-absolute popup-prize-btn popup-prize-btn-${prizeType} 
-              popup-prize-btn-${prizeType}-prize-received-${prizeReceived}
-              ${prizeType === "non-dig" && userAddress === "" ? "disabled" : ""}
-              `}
-            onClick={popupPrizeBtn}
-          >
-            <span>{popupBtnTxt}</span>
-          </button> */}
-          {/*  */}
         </div>
       </div>
     </>
